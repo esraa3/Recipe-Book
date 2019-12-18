84a4affd514daf5f7d9b766f68c312d59cba0f8b
esraa bedir <esraabedir110@gmail.com>
2019-12-18 21:31:26 +0200

    ,,

--- recipeProject/src/app/app.module.ts
+++ recipeProject/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+ //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
